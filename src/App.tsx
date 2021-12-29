7566c0231c632f6fced6fa8b07e37fc210764d2d
ishaq shaik <30292733+ishaqshaik222@users.noreply.github.com>
2021-12-29 23:39:24 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
+import Cluster from "./Maps/cluster";
+import { FieldWorkers } from "./fieldWorkers";
+import ActionDetails from "./Screens/ActionDetails"
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<FieldWorkers />} />
+        <Route path="/details" element={<Cluster />} />
+        <Route path="/actions" element={<ActionDetails />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
